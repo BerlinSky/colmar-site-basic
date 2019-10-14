2285adb54e779a12248402a69b8c80b0140baedc
Berlin Sky <pedroyoung8989@gmail.com>
2019-10-14 12:45:48 -0700

    Updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './app/**/*.html',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  // defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  defaultExtractor: content => content.match(/[\w-/:]*[\w-/:]/g) || []
+})
 module.exports = {
   plugins: [
     require('postcss-import'),
@@ -7,6 +17,10 @@ module.exports = {
       browsers: ['last 2 versions', 'ie 11'],
       cascade: false,
       grid: true
-    })
+    }),
+    // ...process.env.NODE_ENV === 'production'
+    //   ? [purgecss]
+    //   : []
+    purgecss
   ]
-}  
+}
