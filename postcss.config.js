ee5742c53798dce45baac8d7460010e8aef8cf0c
Berlin Sky <pedroyoung8989@gmail.com>
2019-10-13 20:17:55 -0700

    Updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-nested'),
     require('autoprefixer') ({
       browsers: ['last 2 versions', 'ie 11'],
       cascade: false,
       grid: true
     })
   ]
-}
+}  
