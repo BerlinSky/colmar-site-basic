a4e3636bf22d0ad129bd6f60292588a1edaa5e4d
Berlin Sky <pedroyoung8989@gmail.com>
2019-11-03 20:41:45 -0800

    add free SVG implementation

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,30 @@
-// console.log("main.js");
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import {
+  faKey,
+  faDesktop,
+  faFlag,
+  faClone,
+  faUser,
+  faEnvelope,
+  faPhone,
+  faCheck,
+  faCircle,
+  faLongArrowAltDown,
+  faLongArrowAltUp,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faKey,
+  faDesktop,
+  faFlag,
+  faClone,
+  faUser,
+  faEnvelope,
+  faPhone,
+  faCheck,
+  faCircle,
+  faLongArrowAltDown,
+  faLongArrowAltUp,
+)
+
+dom.watch()
